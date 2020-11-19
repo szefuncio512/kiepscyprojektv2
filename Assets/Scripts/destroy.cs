974430615634bd81d9c173a6c82c7c9332ba99c4
szefuncio512 <74043484+szefuncio512@users.noreply.github.com>
2020-11-19 15:48:51 +0100

    Initial commit

---
+++ Assets/Scripts/destroy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Specialized;
+
+private IEnumerator coroutine;
+
+
+
+
+   
+
+void Start()
+{
+
+    {
+        SpawnObstacle(startPosition);
+        coroutine = WaitToSpawn(1f);
+        StartCoroutine(coroutine);
+    }
+
+    void Update()
+
+
+    void SpawnObstacle(BitVector32 lastobjPosition)
+
+    IEnumerator WaitToSpawn(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waiTime);
+            SpawnObstacle(obstaclesOnMap[obstaclesOnMap.Count - 1].transform.position);
+            {
+                Destroy(obstaclesOnMap[0]);
+                obstaclesOnMap.Remove(obstaclesOnMap[0]);
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
+
---
+++ Assets/playerThree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] GameObject obstacleObj;
+    List<GameObject> obstaclesOnMap = new List<GameObject>();
+    void Start()
+    {
+        SpawnObstacle(new Vector3(0f, 0.4f, 20f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SpawnObstacle(obstaclesOnMap[obstaclesOnMap.Count - 1].transform.position);
+        }
+    }
+
+    void SpawnObstacle(Vector3 lastObjPosition)
+    {
+        Vector3 spawnPosition = new Vector3(((int)Random.Range(-1.9f, 1.9f) * 3), lastObjPosition.y, lastObjPosition.z + 10);
+        obstaclesOnMap.Add(Instantiate(obstacleObj, spawnPosition, Quaternion.Euler(0, 0, 0)));
+    }
+}
